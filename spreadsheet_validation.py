bb0b305188f45e1e44a6aebf1ad3ad8fb6813a17
w3-M.S <sandersonmatt@protonmail.com>
2019-11-14 22:04:10 -0400

    Create spreadsheet_validation.py

---
+++ spreadsheet_validation.py
@@ -0,0 +1,21 @@
+#!/bin/bash python3
+
+import xlrd, subprocess, platform
+
+input = open("hosts_ip_spreadsheet", "r")
+output = open("inventory", "a")
+
+invalidPairings = {}
+
+def validatePairing():
+  pass
+  
+def pingHost():
+  pass
+  
+def updateSpreadsheet():
+  pass
+
+def logWrite():
+  pass
+
